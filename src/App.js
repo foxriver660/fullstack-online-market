b6bc952447816b120a300329d9fcd29f5dee42aa
Ivan <foxriver660@gmail.com>
2023-02-17 11:03:34 +0300

    style/add basic styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
-
+// COMPONENTS
 import { Header, Footer } from "./components/index";
+// PAGES
 import { HomePage, ContactPage, CardPage, AdminPage, OrderPage } from "./pages";
 
 function App() {
