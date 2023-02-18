a217faa657f06096d2623ed7986b367aab0192cf
Ivan <foxriver660@gmail.com>
2023-02-18 13:03:54 +0300

    feature/add logging

--- src/pages/Auth/LoginPage.jsx
+++ src/pages/Auth/LoginPage.jsx
@@ -1,39 +1,77 @@
 import React from "react";
 import classes from "./Auth.module.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
-import {Card} from '../../components/index'
+import { Card } from "../../components/index";
 import loginImg from "../../images/kisspng-offer-and-acceptance-contract-of-sale-proposal-zak-inventory-management-software-5b1e5574946513.3219275815287146126079.jpg";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase/config";
+import { ToastContainer, toast } from "react-toastify";
+import Loader from "../../components/Loader/Loader";
 const LoginPage = () => {
-  return (
-    <section className={`container ${classes.auth}`}>
-      <div className={classes.img}>
-        <img src={loginImg} alt="login" width="200px" />
-      </div>
-      <Card>
-      <div className={classes.form}>
-       <h2 className={classes.title}>Войти в личный кабинет</h2>
-        <form>
-          <input type="text" placeholder="Email" required />
-          <input type="password" placeholder="Password" required />
-          <button className="--btn --btn-primary --btn-block" type="submit">
-            Войти
-          </button>
-          <span className={classes.register}>
-            Забыли пароль?<Link to="/reset">Восстановить пароль</Link>
-          </span>
-        </form>
-        <button className="--btn --btn-google --btn-block" type="submit">
-          <FcGoogle /> Login with Google
-        </button>
-        <span className={classes.register}>
-          Отсуствует аккаунт?<Link to="/register"> Регистрация</Link>
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const navigate = useNavigate();
+  const loginUser = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
 
-        </span>
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setIsLoading(false);
+        toast.success(`Вы вошли!`);
         
-      </div>
-      </Card>
-    </section>
+      })
+      .catch((error) => {
+        toast.error(error.message);
+        setIsLoading(false);
+      });
+  };
+  return (
+    <>
+      <ToastContainer />
+      {isLoading && <Loader />}
+      <section className={`container ${classes.auth}`}>
+        <div className={classes.img}>
+          <img src={loginImg} alt="login" width="200px" />
+        </div>
+        <Card>
+          <div className={classes.form}>
+            <h2 className={classes.title}>Войти в личный кабинет</h2>
+            <form onSubmit={loginUser}>
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <button className="--btn --btn-primary --btn-block" type="submit">
+                Войти
+              </button>
+              <span className={classes.register}>
+                Забыли пароль?<Link to="/reset">Восстановить пароль</Link>
+              </span>
+            </form>
+            <button className="--btn --btn-google --btn-block" type="submit">
+              <FcGoogle /> Login with Google
+            </button>
+            <span className={classes.register}>
+              Отсуствует аккаунт?<Link to="/register"> Регистрация</Link>
+            </span>
+          </div>
+        </Card>
+      </section>
+    </>
   );
 };
 
