d761bc3a65e26875d2c6469d631c20af095ec0c4
Ivan <foxriver660@gmail.com>
2023-02-20 17:02:57 +0300

    feature/add navbar on admin page

--- src/components/Admin/NavBar/NavBar.jsx
+++ src/components/Admin/NavBar/NavBar.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import classes from './NavBar.module.scss'
+import {FaUserCircle} from 'react-icons/fa'
+import { useSelector } from 'react-redux/es/hooks/useSelector'
+import { selectUserName } from '../../../redux/slice/authSlice'
+import {NavLink} from 'react-router-dom'
 
+const activeLink = ({ isActive }) =>
+  isActive ? classes.active : classes.navLink;
 const NavBar = () => {
+  const userName = useSelector(selectUserName)
+  
   return (
-    <div>
-      <h1>NavBar</h1>
+    <div className={classes.navbar}>
+      <div className={classes.user}>
+        <FaUserCircle size={40} color='#fff'/>
+<h4 className={classes.userName}>{userName}</h4>
+      </div>
+      <nav className={classes.nav}>
+      <ul className={classes.navList}>
+        <li className={classes.navItem}>
+          <NavLink to="/admin/home" className={activeLink}>Home</NavLink>
+        </li>
+        <li className={classes.navItem}>
+          <NavLink to="/admin/viewproducts" className={activeLink}>View products</NavLink>
+        </li>
+        <li className={classes.navItem}>
+          <NavLink to="/admin/addproducts" className={activeLink}>Add products</NavLink>
+        </li>
+        <li className={classes.navItem}>
+          <NavLink to="/admin/orders" className={activeLink}>Orders</NavLink>
+        </li>
+      </ul>
+      </nav>
     </div>
   )
 }
