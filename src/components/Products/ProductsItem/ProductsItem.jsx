fcca7e2f140915caab7ed55d203c8b4b393e775b
Ivan <foxriver660@gmail.com>
2023-02-28 12:40:19 +0300

    feature/add productItem comp.

--- src/components/Products/ProductsItem/ProductsItem.jsx
+++ src/components/Products/ProductsItem/ProductsItem.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import classes from './ProductsItem.module.scss'
-const ProductsItem = () => {
-  return (
-    <div>
-      ProductsItem
-    </div>
-  )
-}
+import React from "react";
+import classes from "./ProductsItem.module.scss";
+import Card from "../../Card/Card";
+import { Link } from "react-router-dom";
+const ProductsItem = ({ grid, product, id, name, price, desc, imageURL }) => {
+  // РЕГУЛИРУЕМ ДЛИНУ ТЕКСТА
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      return text.substring(0, n) + "...";
+    } else {
+      return text;
+    }
+  };
 
-export default ProductsItem
+  return (
+    <Card className={grid ? classes.grid : classes.list}>
+      <Link to="/product-details">
+        <div className={classes.img}>
+          <img src={imageURL} alt={name} />
+        </div>
+      </Link>
+      <div className={classes.content}>
+        <div className={classes.details}>
+          <p>{price} &#8381;</p>
+          <h4>{shortenText(name, 15)}</h4>
+        </div>
+        {!grid && <p className={classes.desc}>{shortenText(desc, 200)}</p>}
+        <button type="button" className="--btn --btn-danger">
+          Добавить в корзину
+        </button>
+      </div>
+    </Card>
+  );
+};
+
+export default ProductsItem;
--- src/components/Products/ProductsList/ProductsList.jsx
+++ src/components/Products/ProductsList/ProductsList.jsx
@@ -1,41 +1,55 @@
-import React, { useState } from 'react'
-import classes from './ProductsList.module.scss'
-import {BsFillGridFill} from 'react-icons/bs'
-import {FaListAlt} from 'react-icons/fa'
-import Search from '../../Search/Search'
+import React, { useState } from "react";
+import classes from "./ProductsList.module.scss";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+import Search from "../../Search/Search";
+import ProductsItem from "../ProductsItem/ProductsItem";
 
-
-const ProductsList = ({products}) => {
-  const [grid, setGrid] = useState(true)
-  const [search, setSearch] = useState('')
-
+const ProductsList = ({ products }) => {
+  const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
 
   return (
     <div className={classes.product_list}>
-      <div className={classes.top} id='product'>
+      <div className={classes.top} id="product">
         <div className={classes.icons}>
-          <BsFillGridFill size={22} onClick={() => setGrid(true)}/>
-          <FaListAlt size={22} onClick={() => setGrid(false)}/>
-          <p><b>{`${products.length} `}</b> Товаров найдено.</p>
+          <BsFillGridFill size={22} onClick={() => setGrid(true)} />
+          <FaListAlt size={22} onClick={() => setGrid(false)} />
+          <p>
+            <b>{`${products.length} `}</b> Товаров найдено.
+          </p>
         </div>
         {/* SEARCH */}
         <div>
-          <Search value={search} onChange={(e)=>setSearch(e.target.value)}/>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
         </div>
         {/* SORT PRODUCT */}
         <div className={classes.sort}>
           <label>Сортировать по:</label>
           <select>
-            <option value='latest'>Новинки</option>
-            <option value='lowest-price'>Низкая цена</option>
-            <option value='highest-price'>Высокая цена</option>
-            <option value='a-z'>A-Z</option>
-            <option value='z-a'>Z-A</option>
+            <option value="latest">Новинки</option>
+            <option value="lowest-price">Низкая цена</option>
+            <option value="highest-price">Высокая цена</option>
+            <option value="a-z">A-Z</option>
+            <option value="z-a">Z-A</option>
           </select>
-  </div>
+        </div>
+      </div>
+      <div className={grid ? classes.grid : classes.list}>
+        {products.length === 0 ? (
+          <p>Товары не найдены</p>
+        ) : (
+          products.map((product) => {
+            return (
+              <div key={product.id}>
+                <ProductsItem {...product} grid={grid} product={product} />
+              </div>
+            );
+          })
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsList
+export default ProductsList;
