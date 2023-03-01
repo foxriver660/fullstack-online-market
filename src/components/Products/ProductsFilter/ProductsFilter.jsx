09caaa89d1048cc7d89b6010455e6d414aaa32c3
Ivan <foxriver660@gmail.com>
2023-03-01 16:32:23 +0300

    feature/add product filter comp.

--- src/components/Products/ProductsFilter/ProductsFilter.jsx
+++ src/components/Products/ProductsFilter/ProductsFilter.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
-import classes from './ProductsFilter.module.scss'
+import React from "react";
+import classes from "./ProductsFilter.module.scss";
 const ProductsFilter = () => {
   return (
-    <div >
-      <h3>ProductsFilter</h3>
+    <div className={classes.filter}>
+      <h4 className={classes.title}>Категория</h4>
+      <div className={classes.category}>
+        <button className={classes.btn}>Все</button>
+      </div>
+      <h4 className={classes.title}>Брэнд</h4>
+      <div className={classes.brand}>
+      <select name="brand">
+        <option value="all">Все</option>
+      </select>
+      </div>
+      <h4 className={classes.title}>Стоимость</h4>
+      <p>1500</p>
+      <div className={classes.price}>
+        <input name="price" type="range" min={10000} max={200000} />
+      </div>
+      <br/>
+      <button className="--btn --btn-primary">Очистить фильтр</button>
     </div>
-  )
-}
+  );
+};
 
-export default ProductsFilter
+export default ProductsFilter;
