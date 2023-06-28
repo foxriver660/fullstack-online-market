9ce85e3cacdd2fe61e53c408e030718982f32631
Ivan <foxriver660@gmail.com>
2023-06-28 14:22:17 +0300

    fix 1

--- src/components/Basket/Basket.jsx
+++ src/components/Basket/Basket.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import classes from "./Basket.module.scss";
 import { Link } from "react-router-dom";
 import { BsCart4 } from "react-icons/bs";
